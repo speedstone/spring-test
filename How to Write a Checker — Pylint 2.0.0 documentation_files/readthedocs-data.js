0cf82127246af469d6a3d4cf04ee815fcd2da858
zhenglei <756284800@qq.com>
2018-03-15 23:42:32 +0800

    add new html

---
+++ How to Write a Checker — Pylint 2.0.0 documentation_files/readthedocs-data.js
@@ -0,0 +1,15 @@
+  var READTHEDOCS_DATA = {
+    project: "pylint",
+    version: "latest",
+    language: "en",
+    programming_language: "py",
+    subprojects: {},
+    canonical_url: "http://docs.pylint.org/en/latest/",
+    theme: "nature",
+    builder: "sphinx",
+    docroot: "/doc/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "9dd978aa"
+  };
+  
